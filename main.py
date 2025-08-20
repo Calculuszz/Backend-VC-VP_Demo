656ff9f076957d0d12e305d2a7981866417b1680
Calculuszz <thapakornsuwanpho@gmail.com>
2025-08-20 15:12:41 +0700

    dad

--- main.py
+++ main.py
@@ -8,12 +8,9 @@ app = FastAPI(
     description="MVP สำหรับออก JWT-VC + JWKS + revocation (simple)"
 )
 
-# รวม routers
 app.include_router(issuer.router, prefix="/issuer", tags=["issuer"])
 app.include_router(wellknown.router, tags=["well-known"])
 app.include_router(verifier.router, prefix="/verifier", tags=["verifier"])
-
-# การตั้งค่า CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
@@ -21,8 +18,8 @@ app.add_middleware(
         "https://backend-vc-vpdemo-production.up.railway.app",
     ],  # หรือใส่โดเมนโปรดักชัน
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],  # จำกัด methods ที่อนุญาต
-    allow_headers=["Content-Type", "Authorization"],  # จำกัด headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
